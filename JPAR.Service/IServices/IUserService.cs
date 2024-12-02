7998b8e57c8a9b066e9a281c40a9fec8f6d9cd2e
kareman <kareman.mahmoud@sewedy.com.eg>
2024-12-02 22:05:44 +0200

    user identity

--- JPAR.Service/IServices/IUserService.cs
+++ JPAR.Service/IServices/IUserService.cs
@@ -1,6 +1,9 @@
-﻿namespace JPAR.Service.IServices
+﻿using JPAR.Service.DTOs;
+
+namespace JPAR.Service.IServices
 {
     public interface IUserService
     {
+        public bool Register(UserRegistrationDTO user);
     }
 }
